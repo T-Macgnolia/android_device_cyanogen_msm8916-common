81a7ba0943078e50ce591195f7c214f050b448e2
T-Macgnolia <tmacgnolia@gmail.com>
2016-01-20 20:55:13 -0500

    Imported keylout from old msm8916-common
    Old msm8916-common https://github.com/TeamRegular/android_device_lge_msm8916-common

--- msm8916.mk
+++ msm8916.mk
@@ -103,6 +103,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
+
 # Keystore
 PRODUCT_PACKAGES += \
     keystore.msm8916
